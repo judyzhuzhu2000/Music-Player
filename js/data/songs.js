b9d6f34110f0842fd80993aa7be9d26d26b4bc2b
judyzhuzhu2000 <57555753+judyzhuzhu2000@users.noreply.github.com>
2020-11-02 10:38:38 -0800

    change rest of songs url to relative path

--- js/data/songs.js
+++ js/data/songs.js
@@ -7,105 +7,105 @@ export const songsList = [{
   },
   {
     "id": "2",
-    "url": "../../music/Taylor Swift - Love Story.mp3",
+    "url": "music/Taylor Swift - Love Story.mp3",
     "time": "03:56",
     "title": "Taylor Swift - Love Story",
     "artist": "Taylor Swift"
   },
   {
     "id": "3",
-    "url": "../../music/Baby.mp3",
+    "url": "music/Baby.mp3",
     "time": "03:39",
     "title": "Justin Bieber - Baby",
     "artist": "Justin Bieber"
   },
   {
     "id": "4",
-    "url": "../../music/Fight Song.mp3",
+    "url": "music/Fight Song.mp3",
     "time": "03:25",
     "title": "Rachel Platten - Fight Song",
     "artist": "Rachel Platten"
   },
   {
     "id": "5",
-    "url": "../../music/Girls Like You.mp3",
+    "url": "music/Girls Like You.mp3",
     "time": "04:30",
     "title": "Maroon 5 - Girls Like You ft. Cardi BHavana",
     "artist": "Camila Cabello"
   },
   {
     "id": "6",
-    "url": "../../music/Havana.mp3",
+    "url": "music/Havana.mp3",
     "time": "03:38",
     "title": "Havana",
     "artist": "Camila Cabello"
   },
   {
     "id": "7",
-    "url": "../../music/Hero.mp3",
+    "url": "music/Hero.mp3",
     "time": "04:17",
     "title": "Hero",
     "artist": "Mariah Carey "
   },
   {
     "id": "8",
-    "url": "../../music/I'm Yours.mp3",
+    "url": "music/I'm Yours.mp3",
     "time": "04:38",
     "title": "I'm Yours",
     "artist": "Jason Mraz"
   },
   {
     "id": "9",
-    "url": "../../music/perfect.mp3",
+    "url": "music/perfect.mp3",
     "time": "04:23",
     "title": "Perfect",
     "artist": "Ed Sheeran"
   },
   {
     "id": "10",
-    "url": "../../music/Say You Won't Let Go.mp3",
+    "url": "music/Say You Won't Let Go.mp3",
     "time": "03:30",
     "title": "Say You Won't Let Go",
     "artist": "James Arthur"
   },
   {
     "id": "11",
-    "url": "../../music/Westlife - My Love.mp3",
+    "url": "music/Westlife - My Love.mp3",
     "time": "03:59",
     "title": "Westlife-My love",
     "artist": "Westlife"
   },
   {
     "id": "12",
-    "url": "../../music/When You Believe.mp3",
+    "url": "music/When You Believe.mp3",
     "time": "04:57",
     "title": "When You Believe ",
     "artist": "Mariah Carey & Whitney Houston"
   },
   {
     "id": "13",
-    "url": "../../music/You Belong With Me.mp3",
+    "url": "music/You Belong With Me.mp3",
     "time": "03:48",
     "title": "Taylor Swift - You Belong With Me",
     "artist": "Taylor Swift"
   },
   {
     "id": "14",
-    "url": "../../music/太想爱你.mp3",
+    "url": "music/太想爱你.mp3",
     "time": "04:46",
     "title": "張信哲 Jeff Chang [ 太想愛你 ]",
     "artist": "張信哲"
   },
   {
     "id": "15",
-    "url": "../../music/辛曉琪 Winnie Hsin倆倆相忘 Try to forget台視倚天屠龍記插曲.mp3",
+    "url": "music/辛曉琪 Winnie Hsin倆倆相忘 Try to forget台視倚天屠龍記插曲.mp3",
     "time": "03:53",
     "title": "辛曉琪 Winnie Hsin倆倆相忘 ",
     "artist": "辛曉琪"
   },
   {
     "id": "16",
-    "url": "../../music/李麗芬 Lily Lee愛江山更愛美人 The bold and the beautiful台視倚天屠龍記片尾曲.mp3",
+    "url": "music/李麗芬 Lily Lee愛江山更愛美人 The bold and the beautiful台視倚天屠龍記片尾曲.mp3",
     "time": "05:24",
     "title": "李麗芬 Lily Lee愛江山更愛美人",
     "artist": "李麗芬"
